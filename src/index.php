3c4410cc9e42316150cda7381533d25fd019e0d0
Abhay <abhayshukla@cedcommerce.com>
2022-05-10 15:49:06 +0530

    bootstrap

---
+++ src/index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="singlepage.css">
+    <title>Document</title>
+    <style>
+    
+        
+    </style>
+</head>
+<body>
+    <div class="container paGe">
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-sm-2">
+            <h6 style="margin-top: 10px;">Jeveline</h6>
+        </div>       
+<div class="col-sm-10" >
+<ul class="nav justify-content-end">
+  <li class="navbar-item">
+    Home<i class="bi bi-caret-down-fill"></i>
+  <li class="navbar-item">
+    Blog<i class="bi bi-caret-down-fill"></i>
+  </li>
+  <li class="navbar-item">
+    Portfolio<i class="bi bi-caret-down-fill"></i>
+  </li>
+  <li class="navbar-item">
+    Shop<span class="badge rounded-pill bg-danger info shop">New</span><i class="bi bi-caret-down-fill"></i>
+  </li>
+  <li class="navbar-item">
+   Variations<i class="bi bi-caret-down-fill"></i>
+  </li>
+  <li class="navbar-item">
+elements<i class="bi bi-caret-down-fill"></i>
+  </li>
+  <li class="navbar-item"><i class="bi bi-cart"><span class="badge rounded-pill bg-primary info shop quan">0</span></i></li>
+  <li class="navbar-item"><i class="bi bi-search"></i></li>
+</ul>  
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid navigation">
+    <ul class="navigation-ul">
+        <li class="navigation-item">Home<i class="bi bi-chevron-compact-right"></i></li>
+        <li class="navigation-item">Shop <i class="bi bi-chevron-compact-right"></i></li>
+        <li class="navigation-item">Andriod Phone</li>
+    </ul>
+    </div>
+
+
+    <span class="container-fluid iconBar justify-content-end">
+        <i class="bi bi-layers"></i><br>
+        <i class="bi bi-eyeglasses"></i> <br>
+        <i class="bi bi-bag"></i> <br>
+        <i class="bi bi-question-circle"></i> <br>
+        <i class="bi bi-envelope"></i>
+    </span>
+    
+    <div class="container-fluid middle">
+        <div class="row">
+          <div class="col-sm img">
+              <img src="./images/m.png" alt="img"><span class="badge rounded-pill bg-danger info shop" style="position: relative;top: -100px; left: -70px;">Sale!</span>
+          </div>
+
+          <div class="col-sm right">
+            <h5><b>Anroid Phone</b></h5>
+            <div class="row andro">
+                <div class="col-sm">
+                    <p class="text-muted">$199.00 <strong>$199.00 </strong></p>   
+                </div>
+                <div class="col-sm justify-content-end star">
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <strong class="review">(1 review)</strong> 
+                </div>
+            </div>
+            <div class="container-fluid para">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem assumenda, repudiandae officia eos aliquam voluptatibus! Voluptatibus fuga corporis delectus in quod, necessitatibus maiores minus esse aut distinctio vitae debitis quasi?</p>
+            </div>
+
+            <div class="container-fluid addCart">
+                <i class="bi bi-arrow-down-square"></i> &nbsp; 1 &nbsp;<i class="bi bi-arrow-up-square"></i> &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; 
+                <button type="button" style="font-size: xx-small;" class="btn btn-primary bi bi-cart btncart">Add to cart</button>    
+            </div>
+
+            <div class="container-fluid SKU">
+                <b>SKU :</b><span class="text-muted">12</span> <br>
+                <b>Category :</b><a href="#">Phones</a><br>
+                <b>Tag :</b><span class="text-muted">Screen</span><br>
+                <br>
+                <button class="bi bi-share shared">Share</button>
+            </div>
+
+            <div class="container-fluid description_review">
+                <span class="description">Description</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="reviews">Reviews(1)</span>
+                &nbsp;&nbsp;
+                &nbsp;&nbsp;
+                &nbsp;&nbsp;
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci reprehenderit atque nam voluptatem nostrum aspernatur, non quos excepturi. Ratione deleniti deserunt cupiditate maiores in reiciendis laudantium officiis doloribus hic eligendi!</p>
+            </div>
+        </div>
+
+        <div class="container-fluid footer_up">
+            <div class="row">
+                <div class="col-sm">
+                    <i class="bi bi-cart" style="color: blue;" ></i>&nbsp;<span style="font-size: x-small;"><b>Free Shiping</b><p style="position: relative;left: 20px;">for purchase over $50</p></span>
+
+                </div>
+                <div class="col-sm">
+                    <i class="bi bi-shield-x  fa-lg" style="color: blue;" ></i>&nbsp;<span style="font-size: x-small;"><b>Secure Payement</b><p style="position: relative;left: 20px;">for online transactions</p></span>
+                </div>
+                <div class="col-sm">
+                    <i class="bi bi-lightning-charge" style="color: blue;"></i>&nbsp;<span style="font-size: x-small;"><b>Same day shiping</b><p style="position: relative;left: 20px;">for purchase till 6:00 pm ES</p></span>
+                </div>
+
+            </div>
+        </div>
+        <div class="container-fluid footer_head">
+            <div class="row heading_footer">
+                <div class="col-sm">
+                    <h5 style="color: white;"><b>Jevelin</b></h5>
+                </div>
+                <div class="col-sm social">
+                    <i class="bi bi-twitter icon"></i><i class="bi bi-instagram icon"></i><i class="bi bi-youtube icon"></i><i class="bi bi-whatsapp icon"></i>
+                </div>
+            </div>
+<div class="container-fluid terms">
+    <div class="row four">
+        <div class="col-sm">
+            Start Crafting Epic <b>Websites</b> Today! <br>
+            <button type="button" class="btn btn-primary purchaseBtn" style="font-size: xx-small;"><b>Purchase Theme</b></button>
+            </div>
+        <div class="col-sm">
+            <b>Useful</b>
+            <p>documentation intra video showcase Buy theme</p>
+        </div>
+        <div class="col-sm">
+            <b>Keep in touch</b>
+            <p>Nippon,Osaka,Crimson street 78-42(10am-6pm EST,Mon-Fri)</p>
+            <b>support@brand.com</b>
+            
+        </div>
+        <div class="col-sm">
+            <b>signup for newsletter</b>
+            <input type="text" placeholder="E-mail address"><button class="send">send</button>
+            <small>*information is not disclosed to third parties</small>
+        </div>
+        </div>
+        <div class="row">
+            <div class="col-sm shufflehound" >
+                ©2021Jevelin.Crafted by <b>shufflehound</b>
+            </div>
+            <div class="col-sm privacy">
+                <small style="padding: 20px;"><b>privacy policy</b></small><small><b>Term & Conditions</b></small> 
+                </div>
+            </div>
+    </div> -->
+</div>
+        </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+</html>
